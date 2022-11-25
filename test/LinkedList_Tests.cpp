2abfc9f5bd20ba49c63069e5f9c398edf65e50c6
MattIzon <matt.izon@googlemail.com>
2022-11-25 23:33:02 +0000

    Created Empty Test File and Cpp + Hpp Files

---
+++ test/LinkedList_Tests.cpp
@@ -0,0 +1,14 @@
+#include <gtest/gtest.h>
+#include "../LinkedList.hpp"
+
+TEST(Initialisation, No_Values)
+{
+}
+
+TEST(Initialisation, Single_Value)
+{
+}
+
+TEST(Initialisation, List_Of_Values)
+{
+}
